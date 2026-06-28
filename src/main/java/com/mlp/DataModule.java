bf6a32a8b47fdde23545f7cb96dbfa8c6883074e
Matteo Fioretti <mfioretti.inbox@gmail.com>
2026-06-28 11:24:26 +0200

    Implement abstract DataModule with batchSize field and Iterable contract

--- src/main/java/com/mlp/DataModule.java
+++ src/main/java/com/mlp/DataModule.java
@@ -1,6 +1,11 @@
 package com.mlp;
 // Field: batchsize
-// abstrac methods:
+// it implements Iterable<Batch> which forces it to provide and iterator() method
 
-public abstract class DataModule {
+public abstract class DataModule implements Iterable<Batch> {
+    protected int batchSize;
+    public DataModule(int batchSize){
+        this.batchSize = batchSize;
+    }
+
 }
