3f50fc1e40d760ba498a2f3ebdf152bf63e82d52
Matteo Fioretti <mfioretti.inbox@gmail.com>
2026-06-28 16:24:52 +0200

    Implement MNISTDataModule: IDX parsing, one-hot labels, batch iterator

--- src/main/java/com/mlp/MNISTDataModule.java
+++ src/main/java/com/mlp/MNISTDataModule.java
@@ -1,4 +1,90 @@
 package com.mlp;
 
-public class MNISTDataModule {
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class MNISTDataModule extends DataModule {
+    private double[][] X;
+    private double[][] y;
+
+    public MNISTDataModule(int batchSize, String imagePath, String labelPath) throws IOException {
+        super(batchSize);
+        this.X = loadImages(imagePath);
+        this.y = loadLabels(labelPath);
+    }
+    @Override
+    public Iterator<Batch> iterator(){
+        return new BatchIterator();
+    }
+
+    private class BatchIterator implements Iterator<Batch>{
+        private int currentIndex = 0;
+
+        public boolean hasNext(){
+            return currentIndex < X.length;
+        }
+
+        public Batch next(){
+            int currBatchSize = Math.min(batchSize, X.length - currentIndex);
+            double[][] X_batch = Arrays.copyOfRange(X, currentIndex, currentIndex + currBatchSize);
+            double[][] y_batch = Arrays.copyOfRange(y,currentIndex, currentIndex+currBatchSize);
+            currentIndex += currBatchSize;
+            return new Batch(X_batch, y_batch);
+        }
+
+
+    }
+    private double[][] loadImages(String path) throws IOException{
+        // opening the file
+        try(DataInputStream imagePipe = new DataInputStream(
+                new BufferedInputStream(
+                        new FileInputStream(path)))) {
+
+                    int pixels = 784;
+                    int magicNumber = imagePipe.readInt();
+                    int count = imagePipe.readInt();
+                    int rows = imagePipe.readInt();
+                    int cols = imagePipe.readInt();
+
+                    if (magicNumber != 2051) {
+                        throw new IOException("Invalid image file magic number: " + magicNumber);
+                    }
+
+                    double[][] result = new double[count][pixels];
+
+                    for (int i = 0; i < count; i++){
+                        for (int j = 0; j < pixels; j++) {
+                            result[i][j] = imagePipe.readUnsignedByte() / 255.0;
+                        }
+                    }
+                    return result;
+        }
+    }
+
+    private double[][] loadLabels (String path) throws IOException{
+        try(DataInputStream labelPipe = new DataInputStream(
+                new BufferedInputStream(
+                        new FileInputStream(path)))) {
+           int classes = 10;
+           int magicNumber = labelPipe.readInt();
+           int count = labelPipe.readInt();
+
+            if (magicNumber != 2049) {
+                throw new IOException("Invalid label file magic number: " + magicNumber);
+            }
+
+            double[][] result = new double[count][classes];
+
+            for (int i = 0; i < count; i++){
+               int label = labelPipe.readUnsignedByte();
+               result[i][label] = 1;
+           }
+
+            return result;
+        }
+    }
 }
--- src/main/java/com/mlp/Main.java
+++ src/main/java/com/mlp/Main.java
@@ -1,4 +1,18 @@
 package com.mlp;
 
+
+import java.io.IOException;
 public class Main {
+    public static void main(String[] args) throws  IOException {
+        MNISTDataModule trainData = new MNISTDataModule(128,
+                "data/train-images.idx3-ubyte",
+                "data/train-labels.idx1-ubyte");
+
+        int batchCount = 0;
+        for (Batch batch: trainData){
+            batchCount ++;
+        }
+        System.out.println("Total batches: " + batchCount);
+    }
 }
+
